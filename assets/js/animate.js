6dd151f425ada77e57a7b665c286b3548089d106
0xN0TH1N9 <0xN0TH1N9@gmail.com>
2022-09-13 21:30:34 +0900

    Add doongdoong, smooth bookmark and wow

--- assets/js/animate.js
+++ assets/js/animate.js
@@ -1,3 +1,5 @@
+new WOW().init(); // 스크롤이 되었을 때 나타나도록
+
 const beautifulAnimation = (element, animation, prefix = 'animate__') =>
   // We create a Promise and return it
   new Promise((resolve, reject) => {
@@ -6,7 +8,6 @@ const beautifulAnimation = (element, animation, prefix = 'animate__') =>
 
     node.classList.add(`${prefix}animated`, animationName);
 
-    // When the animation ends, we clean the classes and resolve the Promise
     function handleAnimationEnd(event) {
       event.stopPropagation();
       node.classList.remove(`${prefix}animated`, animationName);
@@ -15,8 +16,24 @@ const beautifulAnimation = (element, animation, prefix = 'animate__') =>
 
     node.addEventListener('animationend', handleAnimationEnd, {once: true});
   });
-  
+ 
+// 
 beautifulAnimation('.button-85', 'backInDown').then((message) => {
-    // Do something after the animation
+    //애니메이션 종료 후 동작
     beautifulAnimation('.button-85', 'shakeY')
 });
+
+//smooth bookmark
+let anchorlinks = document.querySelectorAll('a[href^="#"]')
+
+for (let item of anchorlinks) { // relitere 
+    item.addEventListener('click', (e)=> {
+    let hashval = item.getAttribute('href')
+    let target = document.querySelector(hashval)
+    target.scrollIntoView({
+      behavior: 'smooth'
+    })
+    history.pushState(null, null, hashval)
+    e.preventDefault()
+  })
+}
